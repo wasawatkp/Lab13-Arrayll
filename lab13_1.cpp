f1aadd91ec9a2a8ca7b2810c1c82f9bf8294a3a8
wasawatkp <wasawat.upper@gmail.com>
2024-12-30 15:04:09 +0700

    lab13_1 Clear

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,38 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    double sumHar = 0;
+    double Product = 1;
+    double Diff = 0;
+
+    for(int i = 0; i < N; i++) sum += A[i];
+    B[0] = sum/N;
+
+    for(int i = 0; i < N; i++)  Diff += pow(A[i] - B[0],2);
+    B[1] = sqrt(Diff/(N));
+    //cout << "sum = " << sum << endl;
+    //cout << "Diff = " << Diff << endl;
+
+    for(int i = 0; i < N; i++) Product *= A[i];
+    //cout << "Product = " << Product << endl;
+    double n = N;
+    B[2] = pow(Product,1/n);
+
+    for(int i = 0; i < N; i++) sumHar += (1/A[i]);
+    //cout << "sumHar = " << sumHar << endl;
+    B[3] = N/sumHar;
+
+    double max = A[0], min = A[0];
+    for(int i = 0; i < N; i++){
+
+        if(A[i]>max) max = A[i];
+        if(A[i]<min) min = A[i];
+
+    }
+    B[4] = max;
+    B[5] = min;
+
+}
